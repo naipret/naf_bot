b64e37900af5809110cb81f48c33de8deb213d66
naipret <ngvphu235e@gmail.com>
2024-07-04 09:04:20 +0700

    tạm bỏ một số hàm không cần thiết, sắp xếp lại vị trí các lệnh

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open("config.json", 'r') as file:
     config = json.load(file)
     token = config["token"]
 
-class bot(discord.Client):
+class Bot(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
@@ -16,64 +16,53 @@ class bot(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = bot(intents=intents)
+Bot = Bot(intents=intents)
 
-@bot.event
+@Bot.event
 async def on_ready():
     print("")
-    print(f"Your bot {bot.user} is now RUNNING!")
-    print(f"Invite link: https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=8&scope=bot%20applications.commands")
-    await bot.change_presence(activity=discord.Game(name="dsc.gg/nafdiscord"))
+    print(f"Your bot {Bot.user} is now RUNNING!")
+    print(f"Invite link: https://discord.com/api/oauth2/authorize?client_id={Bot.user.id}&permissions=8&scope=bot%20applications.commands")
+    await Bot.change_presence(activity=discord.Game(name="dsc.gg/nafdiscord"))
     print("")
 
-@bot.tree.command(name="ip", description="IP of the Minecraft server.")
+@Bot.tree.command(name="ip", description="IP of the Minecraft server.")
 async def ip(interaction: discord.Interaction) -> None:
     await interaction.response.send_message(f"-> <#1137766486803480686>")
 
-@bot.tree.command(name="invite", description="Invite link of this Discord server.")
+@Bot.tree.command(name="invite", description="Invite link of this Discord server.")
 async def invite(interaction: discord.Interaction) -> None:
     await interaction.response.send_message(f"-> http://dsc.gg/nafdiscord")
     
-@bot.tree.command(name="ping", description="Check the bot's latency.")
-async def ping(interaction: discord.Interaction) -> None:
-    await interaction.response.send_message(f"Pong! {round(bot.latency * 1000)}ms")
+# @bot.tree.command(name="ping", description="Check the bot's latency.")
+# async def ping(interaction: discord.Interaction) -> None:
+#     await interaction.response.send_message(f"Pong! {round(bot.latency * 1000)}ms")
 
-@bot.tree.command(name="echo", description="Echoes the user's message.")
+@Bot.tree.command(name="echo", description="Echoes the user's message.")
 async def echo(interaction: discord.Interaction, message: str) -> None:
     await interaction.response.send_message(message)
 
-@bot.tree.command(name="add", description="Adds two numbers together.")
-async def add(interaction: discord.Interaction, num1: int, num2: int) -> None:
-    await interaction.response.send_message(f"{num1} + {num2} = {num1 + num2}")
+# @bot.tree.command(name="add", description="Adds two numbers together.")
+# async def add(interaction: discord.Interaction, num1: int, num2: int) -> None:
+#     await interaction.response.send_message(f"{num1} + {num2} = {num1 + num2}")
 
-@bot.tree.command(name="subtract", description="Subtracts two numbers.")
-async def subtract(interaction: discord.Interaction, num1: int, num2: int) -> None:
-    await interaction.response.send_message(f"{num1} - {num2} = {num1 - num2}")
+# @bot.tree.command(name="subtract", description="Subtracts two numbers.")
+# async def subtract(interaction: discord.Interaction, num1: int, num2: int) -> None:
+#     await interaction.response.send_message(f"{num1} - {num2} = {num1 - num2}")
 
-@bot.tree.command(name="multiply", description="Multiplies two numbers.")
-async def multiply(interaction: discord.Interaction, num1: int, num2: int) -> None:
-    await interaction.response.send_message(f"{num1} * {num2} = {num1 * num2}")
+# @bot.tree.command(name="multiply", description="Multiplies two numbers.")
+# async def multiply(interaction: discord.Interaction, num1: int, num2: int) -> None:
+#     await interaction.response.send_message(f"{num1} * {num2} = {num1 * num2}")
 
-@bot.tree.command(name="divide", description="Divides two numbers.")
-async def divide(interaction: discord.Interaction, num1: int, num2: int) -> None:
-    if num2 == 0:
-        await interaction.response.send_message("Cannot divide by zero.")
-    else:
-        await interaction.response.send_message(f"{num1} / {num2} = {num1 / num2}")
+# @bot.tree.command(name="divide", description="Divides two numbers.")
+# async def divide(interaction: discord.Interaction, num1: int, num2: int) -> None:
+#     if num2 == 0:
+#         await interaction.response.send_message("Cannot divide by zero.")
+#     else:
+#         await interaction.response.send_message(f"{num1} / {num2} = {num1 / num2}")
 
-@bot.tree.command(name="version", description="Version of the bot.")
-async def version(interaction: discord.Interaction) -> None:
-    await interaction.response.send_message(f"Version 3.8 (05/06/2024)")
-
-@bot.tree.command(name="help", description="Shows this help message.")
-async def help(interaction: discord.Interaction) -> None:
-    embed = discord.Embed(title="Help", description="List of available commands:")
-    for command in bot.tree.get_commands():
-        embed.add_field(name=f"/{command.name}", value=command.description, inline=False)
-    await interaction.response.send_message(embed=embed)
-
-@bot.tree.command(name="guide", description="Send a guide message to a new user.")
-async def guide(interaction: discord.Interaction, user: discord.User) -> None:
+@Bot.tree.command(name="guide", description="Send a guide message to a new user.")
+async def guide(interaction: discord.Interaction, user: discord.User = None):
     guide_message = (
         "_Discord: http://dsc.gg/nafdiscord _\n"
         "_Author: <@812886139593687080> _\n\n"
@@ -94,9 +83,23 @@ async def guide(interaction: discord.Interaction, user: discord.User) -> None:
         "_**Chân thành cảm ơn!**_"
     )
     try:
-        await user.send(guide_message)
-        await interaction.response.send_message(f"Guide message sent to {user.mention}.", ephemeral=True)
+        if user:
+            await user.send(guide_message)
+            await interaction.response.send_message(f"Guide message sent to {user.mention}.", ephemeral = True)
+        else:
+            await interaction.user.send(guide_message)
+            await interaction.response.send_message("Guide message sent to you.", ephemeral = True)
     except discord.Forbidden:
-        await interaction.response.send_message(f"Could not send a message to {user.mention}. They might have DMs disabled.", ephemeral=True)
+        if user:
+            await interaction.response.send_message(f"Could not send a message to {user.mention}. They might have DMs disabled.", ephemeral = True)
+        else:
+            await interaction.response.send_message("Could not send a message to you. You might have DMs disabled.", ephemeral = True)
 
-bot.run(token, reconnect = True)
+@Bot.tree.command(name="help", description="Shows this help message.")
+async def help(interaction: discord.Interaction) -> None:
+    embed = discord.Embed(title="Help", description="List of available commands:")
+    for command in Bot.tree.get_commands():
+        embed.add_field(name=f"/{command.name}", value=command.description, inline=False)
+    await interaction.response.send_message(embed=embed)
+
+Bot.run(token, reconnect = True)
