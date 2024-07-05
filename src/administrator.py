c0c0558545a8eab1d9976448729b90fc12c5d85f
naipret <ngvphu235e@gmail.com>
2024-07-05 15:49:03 +0700

    /msg có thể gửi các file đã có sẵn trong config/message/..., và có thể gửi tin nhắn nhiều dòng

--- src/administrator.py
+++ src/administrator.py
@@ -1,8 +1,20 @@
 import discord
-from discord import app_commands
+import os
 
 
 def setup(bot):
+    def available_file(message: str) -> bool:
+        message_dir = "config/message/"
+        if not os.path.exists(message_dir):
+            return False
+        files = os.listdir(message_dir)
+        message_list = [
+            os.path.splitext(file)[0]
+            for file in files
+            if os.path.isfile(os.path.join(message_dir, file))
+        ]
+        return message in message_list
+
     @bot.tree.command(
         name="msg",
         description="Send a private message to a user.",
@@ -12,12 +24,20 @@ def setup(bot):
         user: discord.User,
         message: str,
     ):
-        message = message.replace("\n")
+        if available_file(message):
+            with open(
+                f"config/message/{message}.txt",
+                "r",
+                encoding="utf-8",
+            ) as file:
+                message = file.read()
+        else:
+            message = message.replace("\\n", "\n")
         if interaction.user.guild_permissions.administrator:
             try:
                 await user.send(message)
                 await interaction.response.send_message(
-                    f"Guide message sent to {user.mention}.",
+                    f"Message sent to {user.mention}.",
                     ephemeral=True,
                 )
             except discord.Forbidden:
@@ -27,7 +47,7 @@ def setup(bot):
                 )
         else:
             await interaction.response.send_message(
-                "You do not have permission to send guide messages to other users.",
+                "You do not have permission to send this message to other users.",
                 ephemeral=True,
             )
 
