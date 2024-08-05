1f175b5fd4d0702f8089d1f714488b2ad2ecd382
naipret <ngvphu235e@gmail.com>
2024-08-05 15:24:57 +0700

    định dạng lại file member_event.py

--- src/member_event.py
+++ src/member_event.py
@@ -1,7 +1,10 @@
 import discord
 
 
-async def on_member_join(member: discord.Member, welcome_channel_id):
+async def on_member_join(
+    member: discord.Member,
+    welcome_channel_id,
+):
     channel = member.guild.get_channel(welcome_channel_id)
     if channel is not None:
         await channel.send(
@@ -11,7 +14,10 @@ async def on_member_join(member: discord.Member, welcome_channel_id):
         print(f"Could not find the welcome channel with ID:", welcome_channel_id)
 
 
-async def on_member_remove(member: discord.Member, goodbye_channel_id):
+async def on_member_remove(
+    member: discord.Member,
+    goodbye_channel_id,
+):
     channel = member.guild.get_channel(goodbye_channel_id)
     if channel is not None:
         await channel.send(
