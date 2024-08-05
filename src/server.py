9511c44327ef449e3745186cbf44d1e856cae430
naipret <ngvphu235e@gmail.com>
2024-08-05 14:21:32 +0700

    sử lại đường dẫn cho /ip và thêm /minigame cho server minigame

--- src/server.py
+++ src/server.py
@@ -4,10 +4,21 @@ import discord
 def setup(bot):
     @bot.tree.command(
         name="ip",
-        description="IP of the Minecraft server.",
+        description="IP of the SMP server.",
     )
     async def ip(interaction: discord.Interaction):
-        await interaction.response.send_message(f"-> <#1137766486803480686>")
+        await interaction.response.send_message(
+            f"-> https://discord.com/channels/998500551488708618/1129023879105499177/1130148231553241092"
+        )
+
+    @bot.tree.command(
+        name="minigame",
+        description="IP of the MINIGAME server.",
+    )
+    async def minigame(interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"-> https://discord.com/channels/998500551488708618/1267736839976910951/1267743994486718475"
+        )
 
     @bot.tree.command(
         name="invite",
